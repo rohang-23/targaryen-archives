a1e8fc11ce2d1e40b838ba6ea9a064047392efb8
Rohan Ishwar Gude <rohangude98@gmail.com>
2026-08-22 17:28:38 +0530

    Add Targaryen Family Relationships

---
+++ data/relationships.sql
@@ -0,0 +1,10 @@
+USE targaryen_archives;
+
+INSERT INTO relationships
+(person_id, related_person_id, relationship_type)
+VALUES
+(1, 4, 'PARENT'),
+(1, 5, 'PARENT'),
+(2, 4, 'PARENT'),
+(2, 5, 'PARENT'),
+(3, 4, 'PARENT');
