43f020d8f1be6a72fc28671c0f6c37dcd2739a5b
Rohan Ishwar Gude <rohangude98@gmail.com>
2026-09-01 19:28:53 +0530

    Complete ruler reign queries

--- queries/05_rulers_queries.sql
+++ queries/05_rulers_queries.sql
@@ -7,4 +7,13 @@ SELECT
 FROM rulers r
 JOIN characters c
     ON r.character_id = c.character_id;
-    
+
+SELECT
+    c.name,
+    r.start_year,
+    r.end_year,
+    r.end_year - r.start_year AS reign_period
+FROM rulers r
+JOIN characters c
+    ON r.character_id = c.character_id
+WHERE r.end_year - r.start_year > 5;
